4fe108d51df0d8d427f5cbdeaa3deab39ec1c6b0
Suravi Mandal <53340870+suravimandal@users.noreply.github.com>
2020-04-04 15:38:06 +0800

    Update apps.py

--- helloworld/apps.py
+++ helloworld/apps.py
@@ -5,3 +5,5 @@ from django.apps import AppConfig
 
 class HelloworldConfig(AppConfig):
     name = 'helloworld'
+    
+    return Response(json.dumps({'Output': 'Hello World from Codestar'}), mimetype='application/json', status=200)
